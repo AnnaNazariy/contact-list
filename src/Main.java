b4d63f6de79bc7e61ce434acc4fa12b2be13f198
AnnaNazariy <125876485+AnnaNazariy@users.noreply.github.com>
2023-06-29 14:15:55 +0300

    Зберігла перший варіант коду

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import youtube.One;
+
+public class Main {
+    public static void main(String[] args) {
+
+        One.umpalumpa();
+
+    }
+}
+
---
+++ src/practical5/showAll.java
@@ -0,0 +1,17 @@
+package practical5;
+
+import java.util.List;
+
+public class showAll{
+
+    public void show(List<Contact> contactList) {
+
+        System.out.println("All yor contact");
+        for (int i = 0; i < contactList.size(); i ++ ){
+            System.out.println("\t" + (i+1) + ". " + contactList.get(i) );
+        }
+
+    }
+
+
+}
